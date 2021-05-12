64230bd066eb9e66c223e12fd82653bb8fccffaf
Phil Barton <phil.barton@digital.homeoffice.gov.uk>
2021-05-12 13:01:41 +0100

    HOCS-2793 - Add team to deployment and secret

--- scripts/complaintIdentitySecret.sh
+++ scripts/complaintIdentitySecret.sh
@@ -3,8 +3,12 @@
 # This script is a template for creating the complaint identity secret
 # After creating a "system user" as the identity for UKVI complaint creation, 
 # the userId and group should be extracted from keycloak and added to this secret
+# The team UUID is taken from the `team` table in the info schema.
+# It is the team associated with the display_name: "CCH Webform Cases"
+  
 
 kubectl create secret generic hocs-delta-ukvi-complaint-identity \
     --from-literal=user=<userId> \
+    --from-literal=team=<teamId> \
     --from-literal=group=<groupId>
     
